582ea3382a46f12106167620acb4b781351f1267
lusciousdev <admin@luscious.dev>
2024-06-22 14:11:07 -0500

    start work on twitch access token auth flow

--- botmanager/models.py
+++ botmanager/models.py
@@ -1,16 +1,18 @@
 from django.db import models
+from django import forms
 import django.contrib.admin as admin
 from django.dispatch import receiver
 
 import datetime
 
 # Create your models here.
+
 class ChatBot(models.Model):
   name = models.CharField(max_length = 256, blank = False, null = False, unique = True)
   
   discord_bot = models.BooleanField(default = True)
   discord_token = models.CharField(max_length = 256, blank = True, null = True)
-  discord_restrict_channels = models.BooleanField(name = "Only post in listed channels?", default = False)
+  discord_restrict_channels = models.BooleanField(default = False)
   
   twitch_bot = models.BooleanField(default = True)
   twitch_client_id = models.CharField(max_length = 256, blank = True, null = True)
@@ -50,8 +52,27 @@ class BotCommandInline(admin.TabularInline):
   model = BotCommand
   extra = 1
   
+class ChatBotForm(forms.ModelForm):
+  template = "botmanager/chat_bot_form.html"
+  
+  name = forms.CharField(max_length = 256)
+  
+  discord_bot = forms.BooleanField()
+  discord_token = forms.CharField(max_length = 256)
+  discord_restrict_channels = forms.BooleanField(label = "Restrict to specific channels?")
+  
+  twitch_bot = forms.BooleanField()
+  twitch_client_id = forms.CharField(max_length = 256)
+  twitch_client_secret = forms.CharField(max_length = 256)
+  twitch_access_token = forms.CharField(max_length = 256)
+  
+  class Meta:
+    model = ChatBot
+    exclude = []
+  
 class ChatBotAdmin(admin.ModelAdmin):
   list_display = ('name', )
   search_fields = [ 'name' ]
+  form = ChatBotForm
   inlines = ( DiscordChannelInline, TwitchChatInline, BotCommandInline, )
   ordering = ( 'name', )
